66f5d1e3f744b9018055e17bd002f2a6e4647777
Otto Sterner <ottosterner@Ottos-MacBook-Air.local>
2024-09-09 19:07:37 +0100

    initial working version

--- src/recommendation-email-automation.py
+++ src/recommendation-email-automation.py
@@ -1,46 +1,119 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import pandas as pd
 
-print("Hello")
-
-
-def download_excel_file(drive, file_id, output_file):
-    file = drive.CreateFile({'id': file_id})
-    file.GetContentFile(output_file, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-def authenticate_drive():
-    gauth = GoogleAuth()
+# Function definitions
+def send_email(to, cc, subject, body):
+    """
+    This function sends an email to the specified recipient with the specified subject and body
     
-    # Ensure this path is correct and the file exists
-    settings_file = 'config/client_secrets.json'
-    credentials_file = 'config/my_credentials.json'
+    Arguments:
+    to: str: The email address of the recipient
+    cc: list: A list of email addresses to CC
+    subject: str: The subject of the email
+    body: str: The body of the email
+
+    Returns:
+    None
+    """
+    from_email = "headcoach@wiltontennisclub.co.uk"
     
-    if not os.path.exists(settings_file):
-        raise FileNotFoundError(f'Client secrets file not found: {settings_file}')
-    
-    # Load client configuration from file
+    # Read the password from a file
+    with open('config/email_password.txt', 'r') as file:
+        password = file.read().strip()
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to
+    msg['CC'] = ", ".join(cc)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
     try:
-        gauth.LoadClientConfigFile(settings_file)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_email, password)
+        text = msg.as_string()
+        server.sendmail(from_email, [to] + cc, text)
+        server.quit()
+        print(f"Email sent to {to}")
     except Exception as e:
-        raise ValueError(f'Failed to load client config file: {e}')
+        print(f"Failed to send email to {to}: {e}")
+        exit()
+
+def get_contacts(file_path, data_columns):
+    """
+    This function reads the contacts from the specified file and returns a dataframe
     
-    # Load saved credentials
+    Arguments:
+    file_path: str: The path to the contacts file
+    data_columns: list: A list of column names to read from the file
+
+    Returns:
+    contacts_df: DataFrame: A pandas DataFrame containing the contacts
+    """
     try:
-        gauth.LoadCredentialsFile(credentials_file)
+        contacts_df = pd.read_csv(file_path, usecols=data_columns)
     except Exception as e:
-        raise ValueError(f'Failed to load credentials file: {e}')
+        print(f"Failed to read contacts from {file_path}: {e}")
+        exit()
+
+    return contacts_df
+
+def loop_through_each_contact_send_email(contacts_df, mode):
+    """
+    This function loops through each contact in the contacts dataframe and sends an email
     
-    # Check if credentials are missing or expired
-    if gauth.credentials is None or gauth.credentials.access_token_expired:
-        if gauth.credentials is None:
-            print("No credentials found. Starting authentication.")
-        elif gauth.credentials.access_token_expired:
-            print("Credentials expired. Refreshing token.")
-        
-        try:
-            gauth.LocalWebserverAuth()  # This will open a browser window for authentication
-            gauth.SaveCredentialsFile(credentials_file)
-        except Exception as e:
-            raise RuntimeError(f'Failed to authenticate or save credentials: {e}')
+    Arguments:
+    contacts_df: DataFrame: A pandas DataFrame containing the contacts
+    mode: str: The mode of operation ('live' or 'self')
+
+    Returns:
+    None
+    """
+
     
-    return GoogleDrive(gauth)
+    for index, row in contacts_df.iterrows():
+        full_name = row['full name']
+        recommendation = row['recommendation']
+
+
+        if mode == "self":
+            email = "ottosterner1@gmail.com"
+            cc_address = [""]
+        else:
+            email = row['email']
+            cc_address = ["headcoach@wiltontennisclub.co.uk"]
+
+        subject = f"TEST!: NEW RECOMMENDATION!"
+        body = (
+            f"Dear Parent or Guardion,\n\n"
+            f"Your child {full_name} has a new recommendation for {recommendation}.\n\n"
+
+            f"Kind regards,\n"
+            f"Marc Beckles, Head Coach"
+        )
+
+        send_email(email, cc_address, subject, body)
+
+
+# Main code
+if __name__ == "__main__":
+    CONTACTS_FILE_PATH = "data/contacts.csv"
+    DATA_COLUMNS = ["full name", "email", "recommendation"]
+
+    # Prompt the user for the mode
+    mode = input("Would you like to run the reminders live or for yourself? (live/self): ").strip().lower()
+
+    contacts_df = get_contacts(CONTACTS_FILE_PATH, DATA_COLUMNS)
+
+    if mode == "live":
+        print("Starting live run...")
+        loop_through_each_contact_send_email(contacts_df, mode)
+    elif mode == "self":
+        print("Starting run for myself...")
+        loop_through_each_contact_send_email(contacts_df, mode)
+    else:
+        print("Invalid mode selected. Please choose 'live' or 'self'.")
